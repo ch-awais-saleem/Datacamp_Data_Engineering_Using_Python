2390331351f8726afc6085c289bbde111d76ac38
Muhammad Awais Saleem <115088021+ch-awais-saleem@users.noreply.github.com>
2022-10-05 18:40:23 +0500

    Create Airflow_DAG.py

---
+++ Airflow_DAG.py
@@ -0,0 +1,14 @@
+# Task definitions
+assemble_frame = BashOperator(task_id="assemble_frame", bash_command='echo "Assembling frame"', dag=dag)
+place_tires = BashOperator(task_id="place_tires", bash_command='echo "Placing tires"', dag=dag)
+assemble_body = BashOperator(task_id="assemble_body", bash_command='echo "Assembling body"', dag=dag)
+apply_paint = BashOperator(task_id="apply_paint", bash_command='echo "Applying paint"', dag=dag)
+
+# Complete the downstream flow
+assemble_frame.set_downstream(place_tires)
+assemble_frame.set_downstream(assemble_body)
+assemble_body.set_downstream(apply_paint)
+
+#Alternate
+assemble_frame >> [place_tires, assemble_body]
+assemble_body >> apply_paint
