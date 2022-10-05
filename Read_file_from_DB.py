3148facc61a42f053017d1fdc59220a3cae2ebdf
Muhammad Awais Saleem <115088021+ch-awais-saleem@users.noreply.github.com>
2022-10-05 18:34:27 +0500

    Create Read_file_from_DB.py

---
+++ Read_file_from_DB.py
@@ -0,0 +1,16 @@
+Read from database
+
+# Function to extract table to a pandas DataFrame
+def extract_table_to_pandas(tablename, db_engine):
+    query = "select * from {}".format(tablename)
+    return pd.read_sql(query,db_engine)
+
+# Connect to the database using the connection URI
+connection_uri = "postgresql://repl:password@localhost:5432/pagila" 
+db_engine = sqlalchemy.create_engine(connection_uri)
+
+# Extract the film table into a pandas DataFrame
+extract_table_to_pandas("film", db_engine)
+
+# Extract the customer table into a pandas DataFrame
+extract_table_to_pandas("customer", db_engine)
