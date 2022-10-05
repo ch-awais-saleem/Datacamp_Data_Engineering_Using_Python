cd1186054a5871efaf0afa38df1b170e271d14e9
Muhammad Awais Saleem <115088021+ch-awais-saleem@users.noreply.github.com>
2022-10-06 03:55:02 +0500

    Create Joining_with_rating.py

---
+++ Joining_with_rating.py
@@ -0,0 +1,27 @@
+# Use groupBy and mean to aggregate the column
+ratings_per_film_df = rating_df.____('____').____('____')
+
+# Join the tables using the film_id column
+film_df_with_ratings = film_df.join(
+    ratings_per_film_df,
+    film_df.film_id==____
+)
+
+# Show the 5 first results
+print(film_df_with_ratings.____)
+
+
+--------------------------------------------------------------------------
+
+
+# Use groupBy and mean to aggregate the column
+ratings_per_film_df = rating_df.groupby('film_id').mean('rating')
+
+# Join the tables using the film_id column
+film_df_with_ratings = film_df.join(
+    ratings_per_film_df,
+    film_df.film_id==ratings_per_film_df.film_id
+)
+
+# Show the 5 first results
+print(film_df_with_ratings.show(5))
